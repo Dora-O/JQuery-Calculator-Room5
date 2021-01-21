0bcee0d1b66f0e474df3ce33dd74aee1599c2728
Jaxon D <duncan5irjax@gmail.com>
2021-01-20 20:47:33 -0600

    Added a base and linked the js to the html

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var firstNumber = '';
 var secondNumber = '';
+var operator = 2;
+var result = 0;
+
+$( document ).ready(function(){
+    $('.btn').on('click', function() { 
+        var click = $(this).val();
+    })
+});
